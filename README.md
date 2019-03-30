79bf5d1018b10194370c68779b9c334516e1a39f
Karthik A <herr.karthik@gmail.com>
2019-03-30 21:24:37 +0530

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # stock_analysis
 Some random algorithms that worked and some that didnt work on the NSE stocks
+Using NSEpy data for analysis
---
+++ code/create_sqlite.py
@@ -0,0 +1,47 @@
+import pandas as pd
+from stock_lib import read_stk_data, read_yaml
+from datetime import timedelta
+import sqlite3 as lite
+from nsepy import get_history
+
+stk_list = read_yaml('../stocks.yaml')
+stk_list.append('NSE/NIFTY_50')
+
+db_file = "stock_data.db"
+conn = lite.connect(db_file)
+cur = conn.cursor()
+
+## This is only for the first time update of the file from hdf5
+# for stk in stk_list:
+#     stk = stk.replace('NSE/','')
+#     print("Processing :"+stk)
+#     df = read_stk_data(stk,2003,2019)
+#     df = df.fillna(method='ffill').fillna('bfill')
+#     df.to_sql(stk,conn,if_exists='replace')
+
+# conn.close()
+
+def update_stk_sql(stk):
+    df = pd.read_sql_query('select * from '+stk,conn, index_col='Date')
+    st_date = pd.to_datetime(df.index[-1])+timedelta(days=1)
+    end_date = pd.to_datetime("today")
+    if stk!='NIFTY_50':
+        df_add = get_history(symbol=stk, start=st_date, end=end_date)
+    else:
+        df_add = get_history(symbol=stk, start=st_date, end=end_date,index=True)
+    df = pd.concat([df,df_add], axis=0)
+    df = df.fillna(method='ffill').fillna('bfill')
+    return df
+
+
+
+
+
+for stk in stk_list:
+    stk=stk.replace('NSE/','')
+    try:
+        df = update_stk_sql(stk)
+        print("Processing : "+stk+" : "+str(df.index[-1]))
+        df.to_sql(stk,conn,if_exists='replace')
+    except:
+        print("Failed : "+stk)
